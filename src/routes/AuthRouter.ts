c91e202e39b0d5f30eab3a8e807e25bf8d86df65
VolcanoCookies <franek.kg@gmail.com>
2022-04-09 18:28:46 +0200

    Changed so redirect is controlled by env vars

--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -5,46 +5,46 @@ import { createToken } from '../utils.js';
 
 const AuthRouter = express.Router();
 // Our own url, used for redirecting
-const __url = process.env.IS_DEV ? 'http://localhost:10105' : 'https://ciam.centralmind.net';
+const __url = process.env.REDIRECT as String;
 const secret = process.env.CLIENT_SECRET as string;
 
 interface SSOResponse {
-    id: string;
-    username: string;
-    avatar: string;
-    discriminator: string;
-    public_flags: number;
-    flags: number;
-    banner: string;
-    accent_color: number;
-    locale: string;
-    mfa_enabled: boolean;
-    premium_type: number;
-    roles: {
-        roles: Array<string>;
-    };
-    originalUrl: string;
+	id: string;
+	username: string;
+	avatar: string;
+	discriminator: string;
+	public_flags: number;
+	flags: number;
+	banner: string;
+	accent_color: number;
+	locale: string;
+	mfa_enabled: boolean;
+	premium_type: number;
+	roles: {
+		roles: Array<string>;
+	};
+	originalUrl: string;
 }
 
 AuthRouter.get('/login', (req, res) => {
-    return res.redirect(`https://sso.isan.to/login?service=${__url}/callback`);
+	return res.redirect(`https://sso.isan.to/login?service=${__url}/callback`);
 });
 
 AuthRouter.get('/callback', async (req, res) => {
-    const code = req.query.code;
+	const code = req.query.code;
 
-    if (!code) return res.sendStatus(401);
+	if (!code) return res.sendStatus(401);
 
-    const dataRes = await fetch(`https://sso.isan.to/getuser/${code}`);
-    const data = (await dataRes.json()) as SSOResponse;
+	const dataRes = await fetch(`https://sso.isan.to/getuser/${code}`);
+	const data = (await dataRes.json()) as SSOResponse;
 
-    if (data.originalUrl != `${__url}/callback`) return res.sendStatus(401);
+	if (data.originalUrl != `${__url}/callback`) return res.sendStatus(401);
 
-    const user = await User.findOne({ 'discord.id': data.id });
+	const user = await User.findOne({ 'discord.id': data.id });
 
-    if (!user) return res.sendStatus(401);
+	if (!user) return res.sendStatus(401);
 
-    res.send(createToken(user));
+	res.send(createToken(user));
 });
 
 export { AuthRouter };
