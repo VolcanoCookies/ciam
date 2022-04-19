ece1bf5ada2add5bca67a8bbba622ef7d036b6fa
VolcanoCookies <franek.kg@gmail.com>
2022-04-19 18:32:36 +0200

    optimized imports and moved some functions to ciam-commons

--- src/utils.ts
+++ src/utils.ts
@@ -1,76 +1,57 @@
-import mongoose from 'mongoose';
-import { User } from './schemas/UserSchema.js';
-import jsonwebtoken from 'jsonwebtoken';
-import { Check } from 'ciam-commons';
+import { flagRegex, strictFlagRegex } from 'ciam-commons/checks';
 import { NextFunction, Request, Response } from 'express';
 import { validationResult } from 'express-validator';
+import jsonwebtoken from 'jsonwebtoken';
+import mongoose from 'mongoose';
+import { User } from './schemas/UserSchema.js';
 
 function stringToObjectIdArray(arr: Array<string>): Array<mongoose.Types.ObjectId> {
 
-    const final = new Array<mongoose.Types.ObjectId>();
+	const final = new Array<mongoose.Types.ObjectId>();
 
-    for (const s of arr) {
-        try {
-            const o = new mongoose.Types.ObjectId(s);
-            final.push(o);
-        } catch (e) { }
-    }
+	for (const s of arr) {
+		try {
+			const o = new mongoose.Types.ObjectId(s);
+			final.push(o);
+		} catch (e) { }
+	}
 
-    return final;
+	return final;
 };
 
-function unique<T>(arr: Array<T>): Array<T> {
-    return arr.filter((v, i, a) => a.indexOf(v) === i);
-}
-
-const __url = process.env.IS_DEV ? 'http://localhost:10105' : 'https://ciam.centralmind.net';
-const secret = process.env.CLIENT_SECRET as string;
-
-enum TokenType {
-    USER = 0,
-    BOT = 1
-}
+const __url = process.env.IS_DEV ? 'http://localhost:10105' : 'https://ciamapi.centralmind.net';
+const secret = process.env.JWT_SECRET as string;
 
 function createToken(user: User): string {
-    const payload = {
-        id: user._id,
-        type: TokenType.USER
-    };
+	const payload = {
+		id: user._id,
+		type: user.type
+	};
 
-    const token = jsonwebtoken.sign(payload, secret, {
-        issuer: __url
-    });
+	const token = jsonwebtoken.sign(payload, secret, {
+		issuer: __url
+	});
 
-    return token as string;
+	return token as string;
 };
 
-function difference<T>(a: Array<T>, b: Array<T>): Array<T> {
-    const arr = new Array<T>();
-    for (const e of a) {
-        if (!b.includes(e)) {
-            arr.push(e);
-        }
-    }
-    return arr;
-}
-
-const objectIdRegex = /[a-f0-9]{24}/;
-
 const flagValidator = function (arr: Array<string>) {
-    return arr.every(f => f.match(Check.flagRegex));
+	return arr.every(f => f.match(flagRegex));
 };
 
 const strictFlagValidator = function (arr: Array<string>) {
-    return arr.every(f => f.match(Check.strictFlagRegex));
+	return arr.every(f => f.match(strictFlagRegex));
 };
 
 const validate = function (req: Request, res: Response, next: NextFunction) {
-    const errors = validationResult(req);
-    if (errors.isEmpty())
-        next();
-    else {
-        res.status(400).send(errors);
-    }
+	const errors = validationResult(req);
+	if (errors.isEmpty())
+		next();
+	else {
+		res.status(400).send(errors);
+	}
 };
 
-export { stringToObjectIdArray, unique, createToken, objectIdRegex, difference, flagValidator, strictFlagValidator, validate };
+type AuthorizedRequest = Request & { user: User; };
+
+export { stringToObjectIdArray, createToken, flagValidator, strictFlagValidator, validate, AuthorizedRequest };
